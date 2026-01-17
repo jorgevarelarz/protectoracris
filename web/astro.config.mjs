ed9f471cf1ba03f7cae167445621df388e84277c
Jorge Varela Ruiz <jorge@MacBook-Pro-de-Jorge.local>
2026-01-17 01:54:44 +0100

    Habilita SSR y analytics astro

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   site: 'https://protectoracris.vercel.app',
+  adapter: vercel(),
   integrations: [tailwind(), sitemap()]
 });
